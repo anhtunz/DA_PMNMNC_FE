c4586fdd5e851350946622a97d95eb11c0381927
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-03-25 00:21:09 +0700

    Add requiresAuth property to routes

--- src/routes/privateRoutes.ts
+++ src/routes/privateRoutes.ts
@@ -4,17 +4,20 @@ import HomePage from '../pages/Home/HomePage'
 interface RouteType {
   path: string
   element: React.ComponentType
+  requiresAuth: boolean
   layout?: React.ComponentType
 }
 export const privateRoutes: RouteType[] = [
   {
     path: '/dashboard',
     element: DashBoardPage,
+    requiresAuth: true,
     layout: MainLayout
   },
   {
     path: '/',
     element: HomePage,
+    requiresAuth: true,
     layout: MainLayout
   }
 ]
