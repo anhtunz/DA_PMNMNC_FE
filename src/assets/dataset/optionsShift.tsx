ca075c52ad23db80bbf4dab73de728faee7490cc
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-04-05 12:10:14 +0700

    Update dataset

---
+++ src/assets/dataset/optionsShift.tsx
@@ -0,0 +1,15 @@
+const optionShift = [
+  {
+    value: 'Ca 1',
+    label: 'Ca 1',
+  },
+  {
+    value: 'Ca 2',
+    label: 'Ca 2'
+  },
+  {
+    value: 'Ca 3',
+    label: 'Ca 3'
+  }
+]
+export { optionShift }
