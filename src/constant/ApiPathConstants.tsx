44191c7b795168d6efd7c188a1026a235d119d37
anhtunz <anhtuanmk62@gmail.com>
2025-04-08 23:11:10 +0700

    Tạo lớp chung lấy data

---
+++ src/constant/ApiPathConstants.tsx
@@ -0,0 +1,7 @@
+class APIPathConstants {
+  static readonly LOGIN_PATH = 'auth/login'
+    static readonly TEST_PATH = 'auth/test'
+    static readonly REFRESH_TOKEN_PATH = 'auth/refresh-token'
+}
+
+export default APIPathConstants
---
+++ src/constant/ApplicationConstant.tsx
@@ -0,0 +1,7 @@
+class ApplicationConstants {
+    static readonly DOMAIN = 'http://127.0.0.1:8000/api'
+    static readonly TOKEN = 'auth_token'
+    
+}
+
+export default ApplicationConstants;
