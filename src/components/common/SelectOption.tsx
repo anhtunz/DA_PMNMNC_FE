41e65da7decaec73a2a86f4d5a63540127fece73
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-04-08 10:35:41 +0700

    Thêm props và sửa ui

--- src/components/common/SelectOption.tsx
+++ src/components/common/SelectOption.tsx
@@ -1,5 +1,6 @@
 import { Select } from 'antd'
 import { useState } from 'react'
+
 interface OptionItem {
   value: string
   label: string
@@ -7,19 +8,23 @@ interface OptionItem {
 
 interface InitialProps {
   optionData: OptionItem[]
+  isMultiSelect: boolean
+  placeholder: string
+  customWidth?: string
 }
-const SelectOption = ({ optionData }: InitialProps) => {
+const SelectOption = ({ optionData, isMultiSelect, placeholder, customWidth }: InitialProps) => {
   const [selected, setSelected] = useState('')
 
   const onChange = (value: string) => {
     setSelected(value)
   }
-  console.log(selected)
   return (
     <Select
       showSearch
-      style={{ minWidth: 120 }}
-      defaultValue={optionData.length > 0 ? optionData[0].value : ''}
+      mode={isMultiSelect ? 'multiple' : undefined}
+      allowClear
+      placeholder={placeholder}
+      style={{ width: `${customWidth}` }}
       optionFilterProp='label'
       onChange={onChange}
       options={optionData}
