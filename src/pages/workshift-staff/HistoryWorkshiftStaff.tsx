ffb00bd5b3faeb438670c23bb98f17cb42995dee
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-04-08 10:43:31 +0700

    Bỏ disable end_date; xử lí khi chọn start_date, end_date

--- src/pages/workshift-staff/HistoryWorkshiftStaff.tsx
+++ src/pages/workshift-staff/HistoryWorkshiftStaff.tsx
@@ -1,77 +1,77 @@
 import SelectOption from '../../components/common/SelectOption'
+import dayjs from 'dayjs'
 import RangeCalendarComponent from '../../components/common/RangeCalendar'
-import { optionShift } from '../../assets/dataset/optionsShift'
 import TableComponent from '../../components/common/TableComponent'
 import { data, columns } from '../../assets/dataset/tableContent'
 import { Button, Popover } from 'antd'
 import { useState } from 'react'
 import { FilterOutlined } from '@ant-design/icons'
+import { optionStaff } from '../../assets/dataset/optionStaff'
+import { formatDateByDMY, formatDateByYMD } from '../../components/helpers/formatNowDate'
 const HistoryWorkshiftStaffPage = () => {
-  const optionStaff = [
-    {
-      value: 'nv1',
-      label: 'Nhân viên A'
-    },
-    {
-      value: 'nv2',
-      label: 'Nhân viên B'
-    },
-    {
-      value: 'nv3',
-      label: 'Nhân viên C'
-    },
-    {
-      value: 'nv4',
-      label: 'Nhân viên D'
-    },
-    {
-      value: 'nv5',
-      label: 'Nhân viên E'
-    },
-    {
-      value: 'nv6',
-      label: 'Nhân viên F'
-    },
-    {
-      value: 'nv7',
-      label: 'Nhân viên G'
-    },
-    {
-      value: 'nv8',
-      label: 'Nhân viên H'
-    },
-    {
-      value: 'nv9',
-      label: 'Nhân viên I'
-    },
-  ]
   const [open, setOpen] = useState(false)
   const handleOpenChange = (newOpen: boolean) => {
     setOpen(newOpen)
   }
+  const handleCallApi = (open: boolean) => {
+    setOpen(!open)
+  }
+
+  /** Xử lí dữ liệu date 
+   * @param {nowFormatYMD} - ngày hiện tại theo định dạng YYYY/MM/DD
+   * @param {nowFormatDMY} - ngày hiện tại theo định dạng DD/MM/YYYY
+   * @param {rangeDate} - khoảng thời gian được chọn
+   * @param {onChange} - hàm xử lí khi chọn khoảng thời gian
+   * @description
+    * `onChange` sẽ nhận vào 2 tham số là `firstDay` và `lastDay`, nếu không có giá trị thì sẽ gán bằng ngày hiện tại.
+    * Nếu `firstDay` hoặc `lastDay` lớn hơn ngày hiện tại thì sẽ gán lại bằng ngày hiện tại.
+  */
   const now = new Date()
-  const date = String(now.getDate()).padStart(2, '0')
-  const month = String(now.getMonth() + 1).padStart(2, '0')
-  const year = now.getFullYear()
-  const day = date + '/' + month + '/' + year
-  //date format: yyyy/mm/dd
-  const dateFormat = year + '/' + month + '/' + date
+  const nowFormatYMD = formatDateByYMD(now)
+  const nowFormatDMY = formatDateByDMY(now)
+  const [rangeDate, setRangeDate] = useState({ startDate: nowFormatYMD, endDate: nowFormatYMD })
+
+  const onChange = (range: { firstDay: string | null; lastDay: string | null }) => {
+    let startDate = range.firstDay ?? nowFormatYMD
+    let endDate = range.lastDay ?? nowFormatYMD
+    let startDateDayjs = dayjs(startDate, 'YYYY-MM-DD');
+    let endDateDayjs = dayjs(endDate, 'YYYY-MM-DD');
+    if (startDateDayjs.isAfter(now)) {
+      startDateDayjs = dayjs(now).startOf('day');
+      startDate = startDateDayjs.format('YYYY-MM-DD');
+    }
+
+    if (endDateDayjs.isAfter(now)) {
+      endDateDayjs = dayjs(now).endOf('day');
+      endDate = endDateDayjs.format('YYYY-MM-DD');
+    }
+    setRangeDate({ startDate, endDate })
+  }
+
   return (
     <div className='flex flex-col shadow-gray-50 bg-white p-6 rounded-2xl'>
       <div className='w-full flex justify-end items-center gap-3 pb-3'>
-        <span className='font-bold'>Ngày hôm nay: {`${day}`}</span>
+        <span className='font-bold'>Ngày hôm nay: {`${nowFormatDMY}`}</span>
         <Popover
           content={
-            <div className='flex justify-center items-end flex-col p-3 gap-3'>
-              <div className='flex flex-col justify-start items-start md:items-start gap-3'>
-                <div className='flex flex-row gap-3'>
-                  <SelectOption optionData={optionStaff} />
-                  <SelectOption optionData={optionShift} />
+            <div className='flex justify-center items-end flex-col p-3 gap-3 sm:w-[400px] max-w-full'>
+              <div className='flex flex-col justify-start items-start md:items-start gap-3 w-full'>
+                <SelectOption
+                  optionData={optionStaff}
+                  isMultiSelect={true}
+                  placeholder='Chọn nhân viên'
+                  customWidth='100%' />
+                <div className='flex flex-col gap-2 w-full'>
+                  <span className='font-bold'>Chọn thời gian</span>
+                  <RangeCalendarComponent
+                    startDate={rangeDate.startDate}
+                    endDate={rangeDate.endDate}
+                    isDisableLastDay={false}
+                    onChange={onChange}
+                  />
                 </div>
-                <div className='flex gap-3'>
-                  <RangeCalendarComponent nowDay={dateFormat} isDisableLastDay={true} /></div>
               </div>
-              <Button type='primary' style={{ width: 'fit-content' }}>
+              <Button type='primary' style={{ width: 'fit-content' }} onClick={() => handleCallApi(open)}>
                 Tìm kiếm
               </Button>
             </div>
@@ -89,11 +89,7 @@ const HistoryWorkshiftStaffPage = () => {
         </Popover>
       </div>
       <div>
-        <TableComponent
-          columns={columns}
-          dataSource={data}
-          pageSizeCustom={5}
-        />
+        <TableComponent columns={columns} dataSource={data} pageSizeCustom={5} />
       </div>
     </div>
   )
