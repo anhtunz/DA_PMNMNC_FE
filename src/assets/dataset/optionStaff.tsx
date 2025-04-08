c4ab357d213bfc35330e6ac0d0120e2a6947dd9c
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-04-08 10:41:25 +0700

    Create optionStaff.tsx

---
+++ src/assets/dataset/optionStaff.tsx
@@ -0,0 +1,46 @@
+export const optionStaff = [
+  {
+    value: 'nv1',
+    label: 'Nhân viên A'
+  },
+  {
+    value: 'nv2',
+    label: 'Nhân viên B'
+  },
+  {
+    value: 'nv3',
+    label: 'Nhân viên C'
+  },
+  {
+    value: 'nv4',
+    label: 'Nhân viên D'
+  },
+  {
+    value: 'nv5',
+    label: 'Nhân viên E'
+  },
+  {
+    value: 'nv6',
+    label: 'Nhân viên F'
+  },
+  {
+    value: 'nv7',
+    label: 'Nhân viên G'
+  },
+  {
+    value: 'nv8',
+    label: 'Nhân viên H'
+  },
+  {
+    value: 'nv9',
+    label: 'Nhân viên I'
+  },
+  {
+    value: 'nv10',
+    label: 'Nhân viên K'
+  },
+  {
+    value: 'nv11',
+    label: 'Nhân viên L'
+  }
+]
