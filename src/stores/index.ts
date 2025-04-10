431620dce7356cc4b767e7f9a0a9d6098feadaf5
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-04-10 22:13:34 +0700

    Update store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { counterSlice } from './slice/counterSlice'
-import { authSlice } from './slice/user/authSlice'
 import { profileSlice } from './slice/user/userProfileSlice'
 export const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
-    auth: authSlice.reducer,
     profile: profileSlice.reducer
   }
 })
