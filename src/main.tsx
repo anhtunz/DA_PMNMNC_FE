8e03b19e6c6569aa57df2a1f6c7387e6551ed33f
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-04-10 13:41:59 +0700

    Refactor file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { store } from './stores/index.ts'
-import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.tsx'
+import { ToastContainer } from 'react-toastify'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
+    <ToastContainer />
   </StrictMode>
 )
