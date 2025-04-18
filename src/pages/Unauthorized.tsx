2acac78bcc2a180e9842f4ea6c37bb8e1a7251c9
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-04-19 00:37:48 +0700

    Update Unauthorized.tsx

--- src/pages/Unauthorized.tsx
+++ src/pages/Unauthorized.tsx
@@ -1,4 +1,16 @@
 const UnauthorizedPage = () => {
-  return <p>unauthorized</p>
+  return <>
+    <section className="bg-white dark:bg-gray-900 h-dvh flex justify-center items-center">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-600 dark:text-primary-500">403</h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Bạn không có quyền truy cập</p>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Xin lỗi, Bạn đã cố truy cập vào một trang mà bạn không được
+            cho phép trước. </p>
+          <a href="/dashboard" className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</a>
+        </div>
+      </div>
+    </section>
+  </>
 }
 export default UnauthorizedPage
