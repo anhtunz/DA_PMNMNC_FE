07a3dfcdef3e32b4570bb764861dcc84609d0434
anhtunz <anhtuanmk62@gmail.com>
2025-04-20 23:07:04 +0700

    Chỉnh sửa base class Network Manager

--- src/config/network_manager.tsx
+++ src/config/network_manager.tsx
@@ -44,7 +44,7 @@ class NetworkManager {
       })
       return response
     } catch (error: any) {
-      return error.response
+      throw error.response
     }
   }
 
@@ -64,7 +64,7 @@ class NetworkManager {
       })
       return response
     } catch (error: any) {
-      return error.response
+      throw error.response
     }
   }
 
@@ -83,7 +83,7 @@ class NetworkManager {
       })
       return response
     } catch (error: any) {
-      return error.response
+      throw error.response
     }
   }
 
@@ -99,7 +99,7 @@ class NetworkManager {
       })
       return response
     } catch (error: any) {
-      return error.response
+      throw error.response
     }
   }
   /**
@@ -109,15 +109,15 @@ class NetworkManager {
    * @param body Contains the data to be updated
    * @returns The HTTP status code of the response
    */
-  async updateDataInServer(path: string, body: Record<string, any>): Promise<number> {
+  async updateDataInServer(path: string, body: Record<string, any>): Promise<any> {
     try {
       console.log(`[${new Date().toLocaleTimeString()}] PUT url: ${ApplicationConstants.DOMAIN}/${path}`)
       const response = await this.axiosInstance.put(path, body, {
         validateStatus: (status) => status === 200
       })
-      return response.status
+      return response
     } catch (error: any) {
-      return error.response
+      throw error.response
     }
   }
 
@@ -127,15 +127,15 @@ class NetworkManager {
    * @param path The endpoint for the request
    * @returns The HTTP status code of the response
    */
-  async deleteDataInServer(path: string): Promise<number> {
+  async deleteDataInServer(path: string): Promise<any> {
     try {
       console.log(`[${new Date().toLocaleTimeString()}] DELETE url: ${ApplicationConstants.DOMAIN}/${path}`)
       const response = await this.axiosInstance.delete(path, {
         validateStatus: (status) => status === 200
       })
-      return response.status
+      return response
     } catch (error: any) {
-      return error.response
+      throw error.response
     }
   }
 }
