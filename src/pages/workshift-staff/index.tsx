912d0a6580ca9dc3cba6abc9b9a76e59ec486ba8
Tuan0209 <buivantuan02092002@gmail.com>
2025-05-11 09:27:40 +0700

    Fix: load tuần tự tránh 'Ca không tồn tại'

--- src/pages/workshift-staff/index.tsx
+++ src/pages/workshift-staff/index.tsx
@@ -1107,11 +1107,31 @@ const WorkshiftStaffPage = () => {
     return null
   }
 
-  useEffect(() => {
-    fetchUsers();
-    fetchShifts();
-    fetchUserShifts();
-  }, []);
+//   useEffect(() => {
+//   setLoading(true)
+//   Promise.all([fetchUsers(), fetchShifts()])
+//     .then(() => setLoading(false))
+// }, [])
+
+
+// useEffect(() => {
+//   if (users.length > 0 && shifts.length > 0) {
+//     fetchUserShifts()
+//   }
+// }, [users, shifts])
+useEffect(() => {
+  const loadData = async () => {
+    setLoading(true)
+    await fetchUsers()
+    await fetchShifts()
+    await fetchUserShifts()
+    setLoading(true)
+    setLoading(false)
+  }
+  loadData()
+}, [])
+
+
 
   return (
     <div className='flex flex-col shadow-gray-50 bg-white p-6 rounded-2xl'>
